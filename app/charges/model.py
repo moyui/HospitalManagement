9b721105b45e7b45f11f0866c534a43dc2cffe5a
moyui <gxy1510@gmail.com>
2018-10-09 14:37:06 +0800

    feat:添加首页模板

--- app/charges/model.py
+++ app/charges/model.py
@@ -3,10 +3,12 @@ from . import db
 class InPatientDeposit(db.model):
     __tablename__ = 'inpatientdeposit'
     id = db.Column(db.Integer, primary_key=True)
+    patientid = db.Column(db.String(10), db.ForeignKey('opcheckin.id'))
     rest = db.Column(db.Float)
 
 class InPatientTotalCost(db.model):
     __tablename__ = 'inpatienttotalcost'
     id = db.Column(db.Integer, primary_key=True)
+    patientid = db.Column(db.String(10), db.ForeignKey('opcheckin.id'))
     totalcost = db.Column(db.Float)
     unpaiditemsid = db.Column(db.String(64)) #未支付项
