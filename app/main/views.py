25732e338a714861e8f159fbb978f9fc69802a24
moyui <gxy1510@gmail.com>
2019-01-13 02:28:24 +0800

    last final

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,4 @@ from ..decorator import is_login
 @main.route('/', methods=['GET', 'POST'])
 @is_login
 def index(name):
-    return render_template('/base.html', name=name)
+    return render_template('/welcome.html', name=name)
